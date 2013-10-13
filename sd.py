a1fb016576f884a157712ba4ba8ec4fa001bc20d
sam <sam.desborough@gmail.com>
2013-10-13 12:24:48 +0100

    More stuff

--- sd.py
+++ sd.py
@@ -23,6 +23,11 @@ def show(template):
     except TemplateNotFound:
         abort(404)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
